4e91e25631838a5a192958dc5358e3f7d0ca522e
Sowparna Shome <soupornoshome@gmail.com>
2024-03-10 23:37:52 +0530

    changed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,7 +4,14 @@ import { useTodo } from "../contexts/TodoContext"
 function TodoItem({todo}) {
     const [isTodoEditable, setIsTodoEditable] = useState(false)
     const [todoMessage, setTodoMessage] = useState(todo.todo)
-    const { updateTodo, deleteTodo, toggleCompleted } = useTodo()
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo()
+    const editTodo = () => {
+        updateTodo(todo.id, { ...todo, todo: todoMessage })
+        setIsTodoEditable(false)
+    }
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
     return (
         <div className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black 
         ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7"}`}>
